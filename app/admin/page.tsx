90c9a574d890102829572d0416883fc27a3a4c4e
williancgromnicki <55885759+williancgromnicki@users.noreply.github.com>
2026-05-03 00:42:29 -0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
-
+export const dynamic = 'force-dynamic';
 import { AlertContactsPanel } from '@/components/AlertContactsPanel';
 import { DataTable } from '@/components/DataTable';
 import { EmptyState } from '@/components/EmptyState';
